ffd85d65680c80fdf80081e52a699f96f572cb38
Novak <novak.t@outlook.com>
2022-06-26 11:13:49 +0200

    Project_4
    Simple bike hire order form created in React. School project.

--- src/App.js
+++ src/App.js
@@ -1,168 +1,363 @@
 import "./App.css";
-import React, { Component } from "react";
-import {
-  PageContainer,
-  Formular,
-  FormSection,
-  MainTitle,
-  SectionTitle,
-  InputDiv,
-} from "./AppStyles";
+import { PageContainer, Form, FormSection, MainTitle, SectionTitle, AccessoriesMain, TypesMain, CheckButton, TotalMain, TotalMainInput, Compare } from "./AppStyles";
 
-import BikeType from "./components/BikeType";
-import Accessories from "./components/Accessories";
-import Calendar from "./components/Calendar";
-import TotalPrice from "./components/TotalPrice";
-import Comparison from "./components/Comparison";
+import { useReducer, useState, useEffect } from "react";
+// initialState pro useReducer
+const defaultOrder = {
+  mountain: false,
+  numMount: 1,
+  kids: false,
+  numKids: 1,
+  road: false,
+  numRoad: 1,
+  gravel: false,
+  numGravel: 1,
+  accessoriesRoof: false,
+  accessoriesTowbar: false,
+  accessoriesNone: false,
+  time: "",
+  budget: 0,
+};
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      totalPrice: 0,
-      days: 5,
-      extras: 1,
-      limit: 0,
-
-      accessories: {
-        height: "190px",
-      },
-      calendar: {
-        height: "163px",
-      },
-      bikes: [
-        {
-          id: "1",
-          label: "Horské",
-          price: 500,
-          count: 0,
-          checked: false,
-        },
-        {
-          id: "2",
-          label: "Dětské",
-          price: 200,
-          count: 0,
-          checked: false,
-        },
-        { 
-          id: "3",
-          label: "Silniční",
-          price: 1500,
-          count: 0,
-          checked: false,
-        },
-        {
-          id: "4",
-          label: "Gravel",
-          price: 2500,
-          count: 0,
-          checked: false,
-        },
-      ],
-    };
-  }
-
-  SelectBike = (event) => {
-    let from = event.target.name;
-    let index = parseInt(event.target.getAttribute("data-pos")) - 1;
-    let bikes = [...this.state.bikes];
-    if (from === "value") {
-      let val = event.target.value;
-      if (val < 1) val = 1;
-      let item = { ...bikes[index], count: val };
-      bikes[index] = item;
-      if (val > 10) val = 10;
-      item = { ...bikes[index], count: val };
-      bikes[index] = item;
-    } else {
-      let val = event.target.checked;
-      let item = { ...bikes[index], checked: val };
-      bikes[index] = item;
-    }
-    this.setState({ bikes: bikes });
-  };
-  NumberOfDays = (event) => {
-    this.setState({ days: event.target.value });
-  };
-  AddExtras = (event) => {
-    this.setState({ extras: event.target.value });
-  };
-
-  TotalSum = () => {
-    let checkedBikes = this.state.bikes.filter((bike) => bike.checked === true);
-    let total = Math.floor(
-      checkedBikes.reduce((a, v) => (a = a + v.price * v.count), 0) *
-        this.state.days *
-        this.state.extras
-    );
-    this.setState({ totalPrice: total });
-  };
-  MaximumLimit = (event) => {
-    this.setState({ limit: event.target.value });
-  };
-
-  render() {
-
-    return (
-      <PageContainer>
-        <Formular>
-          <FormSection>
-            <MainTitle>Kolosalon objednávka</MainTitle>
-          </FormSection>
-          <FormSection name="vyber">
-            <SectionTitle>Výběr kol</SectionTitle>
-            {/* VYBER TYPU KOLA */}
-            <InputDiv>
-              {this.state.bikes.map((bike) => 
-                <BikeType
-                  id={bike.id}
-                  key={bike.id}
-                  label={bike.label}
-                  price={bike.price}
-                  value={bike.count}
-                  checked={bike.checked}
-                  onChange={(event) => this.SelectBike(event)}
-                />
-              )}
-            </InputDiv>
-          </FormSection>
-          <FormSection>
-            {/* VYBER PRISLUSENSTVI */}
-            <SectionTitle>Volba příslušenství</SectionTitle>
-            <InputDiv>
-              <Accessories
-                onChange={(event) => this.AddExtras(event)}
-              />
-            </InputDiv>
-          </FormSection>
-
-          <FormSection>
-            {/* DELKA ZAPUJCKY */}
-            <SectionTitle>Délka zápůjčky</SectionTitle>
-            <InputDiv>
-              <Calendar
-                onChange={(event) => this.NumberOfDays(event)}
-              />
-            </InputDiv>
-          </FormSection>
-
-          <FormSection>
-            {/* VYSLEDNA CENA, SROVNANI */}
-            <SectionTitle>Výsledná cena</SectionTitle>
-            <TotalPrice
-              onClick={() => this.TotalSum()}
-              total={this.state.totalPrice}
-              onChange={(event) => this.AddExtras(event)}
-            />
-            <Comparison
-              onChange={(event) => this.MaximumLimit(event)}
-              limit={this.state.limit}
-              total={this.state.totalPrice}
-            />
-          </FormSection>
-        </Formular>
-      </PageContainer>
-    );
+// reducer funkce pro useReducer
+function setOrder(order, action) {
+  switch (action.type) {
+    case "update_text":
+      return { ...order, [action.key]: action.value };
+    case "update_number":
+      return { ...order, [action.key]: parseFloat(action.value) };
+    case "update_mountain":
+      return { ...order, mountain: !order.mountain };
+    case "update_kids":
+      return { ...order, kids: !order.kids };
+    case "update_road":
+      return { ...order, road: !order.road };
+    case "update_gravel":
+      return { ...order, gravel: !order.gravel };
+    default:
+      return order;
   }
 }
+
+function App() {
+  const [finalPrice, setFinalPrice] = useState(0);
+  const [showFinalPrice, setShowFinalPrice] = useState(0); //need 2nd render using useEffect
+  const [checked, setChecked] = useState(0);
+
+  //state orders managed by useReducer hook
+  const [order, dispatch] = useReducer(setOrder, defaultOrder);
+
+  useEffect(() => {
+    let newFinalPrice = getFinalPrice(order);
+    setShowFinalPrice(newFinalPrice);
+  }, [order]);
+
+  const getFinalPrice = (order) => {
+    let priceMountain = 0;
+    let priceKids = 0;
+    let priceRoad = 0;
+    let priceGravel = 0;
+
+    if (order.mountain) {
+      priceMountain = order.numMount * 500;
+    }
+    if (order.kids) {
+      priceKids = order.numKids * 200;
+    }
+    if (order.road) {
+      priceRoad = order.numRoad * 1500;
+    }
+    if (order.gravel) {
+      priceGravel = order.numGravel * 2500;
+    }
+
+    let thisBasePrice = (priceMountain + priceKids + priceRoad + priceGravel) * order.time;
+    let thisFinalPrice = thisBasePrice;
+
+    if (order.accessories === 0.05) {
+      thisFinalPrice += thisBasePrice * 0.05;
+    } else if (order.accessories === 0.1) {
+      thisFinalPrice += thisBasePrice * 0.1;
+    }
+
+    setFinalPrice(thisFinalPrice);
+    /* console.log(thisFinalPrice); */
+    return thisFinalPrice;
+  };
+
+  const checkPrice = (order) => {
+    console.log(order.budget);
+    if (order.budget >= finalPrice) {
+      let checkOK = 1;
+      setChecked(checkOK);
+    } else {
+      let checkNOK = 2;
+      setChecked(checkNOK);
+    }
+    /* console.log(checked); */
+  };
+
+  return (
+    <PageContainer>
+      <Form>
+        <FormSection name="title">
+          <MainTitle>
+            <u>Bike rental</u>
+          </MainTitle>
+        </FormSection>
+        <FormSection name="options">
+          <SectionTitle>Types of Bicycles</SectionTitle>
+          <TypesMain>
+            <div style={{ textAlign: "center" }}>
+              <label htmlFor="mountain" style={{ cursor: "pointer" }}>
+                {" "}
+                <h4 style={{ marginBottom: 0 }}>Mountain</h4>
+                500 Kc/day
+              </label>
+              <div style={{ textAlign: "center" }}>
+                <input
+                  type="checkbox"
+                  id="mountain"
+                  onChange={() => {
+                    dispatch({
+                      type: "update_mountain",
+                    });
+                  }}
+                />
+                <input
+                  style={{ width: 60 }}
+                  type="number"
+                  min="0"
+                  id="numMount"
+                  value={order.numMount}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "update_number",
+                      value: e.target.value,
+                      key: "numMount",
+                    });
+                  }}
+                />
+              </div>
+            </div>
+            <div style={{ textAlign: "center" }}>
+              <label htmlFor="kids" style={{ cursor: "pointer" }}>
+                {" "}
+                <h4 style={{ marginBottom: 0 }}>Kids</h4>
+                200 Kc/day
+              </label>
+              <div>
+                <input
+                  type="checkbox"
+                  id="kids"
+                  onChange={(e) => {
+                    dispatch({
+                      type: "update_kids",
+                    });
+                  }}
+                />
+                <input
+                  style={{ width: 60 }}
+                  type="number"
+                  id="numKids"
+                  min="0"
+                  value={order.numKids}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "update_number",
+                      value: e.target.value,
+                      key: "numKids",
+                    });
+                  }}
+                />
+              </div>
+            </div>
+            <div style={{ textAlign: "center" }}>
+              <label htmlFor="road" style={{ cursor: "pointer" }}>
+                {" "}
+                <h4 style={{ marginBottom: 0 }}>Road</h4>
+                1500 Kc/day
+              </label>
+              <div>
+                <input
+                  type="checkbox"
+                  id="road"
+                  min="0"
+                  onChange={() => {
+                    dispatch({
+                      type: "update_road",
+                    });
+                  }}
+                />
+                <input
+                  style={{ width: 60 }}
+                  type="number"
+                  id="numRoad"
+                  min="0"
+                  value={order.numRoad}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "update_number",
+                      value: e.target.value,
+                      key: "numRoad",
+                    });
+                  }}
+                />
+              </div>
+            </div>
+
+            <div style={{ textAlign: "center" }}>
+              <label htmlFor="gravel" style={{ cursor: "pointer" }}>
+                {" "}
+                <h4 style={{ marginBottom: 0 }}>Gravel</h4>
+                2500 Kc/day
+              </label>
+              <div>
+                <input
+                  type="checkbox"
+                  id="gravel"
+                  onChange={() => {
+                    dispatch({
+                      type: "update_gravel",
+                    });
+                  }}
+                />
+                <input
+                  style={{ width: 60 }}
+                  type="number"
+                  id="numGravel"
+                  min="0"
+                  value={order.numGravel}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "update_number",
+                      value: e.target.value,
+                      key: "numGravel",
+                    });
+                  }}
+                />
+              </div>
+            </div>
+          </TypesMain>
+        </FormSection>
+        <FormSection name="accessories">
+          <SectionTitle>Accessories</SectionTitle>
+          <AccessoriesMain>
+            <div>
+              <h4 style={{ marginBottom: 2 }}>Roof bike carrier</h4>
+              <input
+                type="radio"
+                name="accessories"
+                id="accessoriesRoof"
+                value={0.05}
+                onChange={(e) => {
+                  dispatch({
+                    type: "update_number",
+                    value: e.target.value,
+                    key: "accessories",
+                  });
+                }}
+              />
+              <label htmlFor="accessoriesRoof" style={{ cursor: "pointer" }}>
+                5% from total price
+              </label>
+            </div>
+            <div>
+              <h4 style={{ marginBottom: 2 }}>Towbar bike rack</h4>
+              <input
+                type="radio"
+                name="accessories"
+                id="accessoriesTowbar"
+                value={0.1}
+                onChange={(e) => {
+                  dispatch({
+                    type: "update_number",
+                    value: e.target.value,
+                    key: "accessories",
+                  });
+                }}
+              />
+              <label htmlFor="accessoriesTowbar" style={{ cursor: "pointer" }}>
+                10% from total price
+              </label>
+            </div>
+            <div>
+              <h4 style={{ marginBottom: 2 }}>Bike carrier</h4>
+              <input
+                type="radio"
+                name="accessories"
+                id="accessoriesNone"
+                value={0}
+                defaultChecked
+                onChange={(e) => {
+                  dispatch({
+                    type: "update_number",
+                    value: e.target.value,
+                    key: "accessories",
+                  });
+                }}
+              />
+              <label htmlFor="accessoriesNone" style={{ cursor: "pointer" }}>
+                Without
+              </label>
+            </div>
+          </AccessoriesMain>
+        </FormSection>
+        <FormSection name="time">
+          <SectionTitle>Duration</SectionTitle>
+          <div style={{ textAlign: "center" }}>
+            <select
+              id="time"
+              style={{ width: 150, textAlign: "center" }}
+              onChange={(e) => {
+                dispatch({
+                  type: "update_text",
+                  value: e.target.value,
+                  key: "time",
+                });
+              }}
+            >
+              <option value={0}>not selected</option>
+              <option value={1}>1 day</option>
+              <option value={2}>2 days</option>
+              <option value={7}>1 week</option>
+              <option value={14}>2 weeks</option>
+            </select>
+          </div>
+        </FormSection>
+        <FormSection name="calculation">
+          <SectionTitle>Total price</SectionTitle>
+          <TotalMain>
+            <label>Final price:</label>
+            <input type="text" id="finalPrice" style={{ width: 140, textAlign: "center", fontSize: "30px" }} value={showFinalPrice} disabled />
+            <label>What is your budget?</label>
+            <TotalMainInput
+              type="number"
+              id="budget"
+              min="0"
+              value={order.budget}
+              onChange={(e) => {
+                dispatch({
+                  type: "update_number",
+                  value: e.target.value,
+                  key: "budget",
+                });
+              }}
+            />
+            <Compare type="text" id="answer" disabled value={checked === 1 ? "Your budget is OK" : "Offer more!"} checked={checked} />
+            <CheckButton
+              checked={checked}
+              onClick={() => {
+                checkPrice(order);
+              }}
+            >
+              Budget checker
+            </CheckButton>
+          </TotalMain>
+        </FormSection>
+      </Form>
+    </PageContainer>
+  );
+}
+
+export default App;
--- src/AppStyles.js
+++ src/AppStyles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+//--------------OBECNE--------------------------------//
 export const PageContainer = styled.div`
   display: flex;
   /* max-width: 1200px; */
@@ -9,69 +10,141 @@ export const PageContainer = styled.div`
   align-items: center;
   align-self: center;
   justify-self: center;
-   
+  background-image: url("/projects/project_4/background-img.jpeg");
+  alt: "background-image";
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
-export const Formular = styled.form`
+export const Form = styled.form`
   width: 800px;
   min-height: 800px;
-  
   padding: 20px;
   display: grid;
   grid-template-columns: 1fr;
   grid-template-areas:
-    "nadpis"
-    "vyber"
-    "prislusenstvi"
-    "dny"
-    "kalkulace";
+    "title"
+    "options"
+    "accessories"
+    "time"
+    "calculation";
   gap: 20px;
 `;
+
 export const FormSection = styled.div`
   display: flex;
   flex-direction: column;
-  border-radius: 25px;
-  
-  
-  /* width: 100%; */
-  background-color: lightgrey;
+  width: auto;
+  background-color: rgba(218, 223, 225, 0.9);
   padding: 20px;
+  border-radius: 20px;
+
   &:nth-child(1) {
-    grid-area: nadpis;
+    grid-area: title;
   }
   &:nth-child(2) {
-    grid-area: vyber;
+    grid-area: options;
   }
   &:nth-child(3) {
-    grid-area: prislusenstvi;
+    grid-area: accessories;
   }
   &:nth-child(4) {
-    grid-area: dny;
+    grid-area: time;
   }
   &:nth-child(5) {
-    grid-area: kalkulace;
+    grid-area: calculation;
   }
 `;
+//----------------------HLAVICKA section--------------------//
 export const SectionTitle = styled.h2`
   color: black;
   font-size: 20px;
   margin: 0px;
   padding: 0;
   padding-bottom: 10px;
+  text-align: center;
+  align-self: center;
   justify-self: center;
-  align-items: center;
 `;
+
 export const MainTitle = styled(SectionTitle)`
   font-size: 30px;
   align-self: center;
   justify-self: center;
-  text-decoration: underline;
 `;
-export const InputDiv = styled.div`
+//-----------------VYBER section---------------------------------//
+export const TypesMain = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-  gap:20px;
+  justify-content: space-evenly;
+  @media only screen and (max-width: 500px) {
+    flex-direction: column;
+  }
+`;
+//-----------------PRISLUSENSTVI section-----------------------------//
+export const AccessoriesMain = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  @media only screen and (max-width: 500px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
+//-----------------VYPOCET section-------------------------------//
+
+export const TotalMain = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TotalMainInput = styled.input`
+  width: 140px;
+  text-align: center;
+  ::-webkit-inner-spin-button,
+  ::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`;
+
+export const CheckButton = styled.div`
+  display: flex;
+  margin-top: 20px;
+  background-color: white;
+  border: 1px solid black;
+  cursor: pointer;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  border-radius: 10px;
+  ${(props) => {
+    if (props.checked === 1) {
+      return `
+            background-color: rgba(0,100,0,0.7);
+         `;
+    } else if (props.checked === 2) {
+      return `
+            background-color: rgba(255,0,0,0.7);
+         `;
+    }
+  }}
+`;
+
+export const Compare = styled.input`
+  font-weight: bold;
+  text-align: center;
+  width: 140px;
+  ${(props) => {
+    if (props.checked === 1) {
+      return `
+            background-color: rgba(0,100,0,0.3);
+
+         `;
+    } else if (props.checked === 2) {
+      return `
+          background-color: rgba(255,0,0,0.3);
+         `;
+    }
+  }}
+`;
